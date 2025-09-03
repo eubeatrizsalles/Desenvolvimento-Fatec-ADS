7eafdeff6069bfcc48f02d7cf19c84c063d3cef0
Beatriz Salles <beatrizsalles008@gmail.com>
2025-09-03 08:55:26 -0300

    Add files via upload

---
+++ bea.python/1calcmedia.py
@@ -0,0 +1,6 @@
+N1 = float (input ("Informe o primeiro número "))
+N2 = float (input ("Informe o segundo número "))
+M = (N1 + N2)/2
+print("a média é", M)
+
+
---
+++ bea.python/2calcmedia.py
@@ -0,0 +1,13 @@
+N1 = float (input ("Informe o primeiro número "))
+N2 = float (input ("Informe o segundo número "))
+M = (N1 + N2)/2
+print("a média é",M)
+if M>= 6:
+    print("aprovado")
+     
+else: 
+    print("reprovado")
+    
+
+    
+    
