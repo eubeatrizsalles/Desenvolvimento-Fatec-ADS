cddd0c5a2c418658148e9bc96b5526f9cac1065a
Beatriz Salles <beatrizsalles008@gmail.com>
2025-09-03 09:37:15 -0300

    Delete bea.python directory

--- bea.python/3horaspmin.py
+++
@@ -1,5 +0,0 @@
-H = int (input("digite as horas "))
-M = int (input("digite os minutos "))
-R = H * 60 
-R1 = R + M
-print("o tempo em minutos é:", R1)
--- bea.python/exercicios.python/4CparaF.py
+++
@@ -1,4 +0,0 @@
-Tc = float (input("Informe a temperatura em Celsius "))
-Tf = 32 + 1.8 * Tc
-print("A temperatura em Fahrenheit é ", Tf)
-
